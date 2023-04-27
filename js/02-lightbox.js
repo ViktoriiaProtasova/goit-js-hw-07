f6e8711f86aeade27ff0c27972ec6e66b0691f93
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-04-27 21:53:03 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryListEl = document.querySelector(".gallery");
+
+window.addEventListener("load", createGallery);
+
+galleryListEl.addEventListener("click", onImageClick);
+
+function createGallery() {
+  const galleryItemsMarkUp = galleryItems
+    .map(
+      (item) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}" />
+   </a>
+</li>`
+    )
+    .join("");
+
+  galleryListEl.insertAdjacentHTML("beforeend", galleryItemsMarkUp);
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  let clickedGalleryImageLink = `${event.target.dataset.source}`;
+
+  showClickedFullSizeImage(clickedGalleryImageLink);
+}
+
+function showClickedFullSizeImage(link) {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    /* options */
+    captionDelay: 250,
+    animationSlide: false,
+  });
+}
 
 console.log(galleryItems);
