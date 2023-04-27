30f0b81dc017bae80822440d0b6c4e9feba56963
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-04-27 21:52:55 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryListEl = document.querySelector(".gallery");
+
+window.addEventListener("load", createGallery);
+
+galleryListEl.addEventListener("click", onImageClick);
+
+function createGallery() {
+  const galleryItemsMarkUp = galleryItems
+    .map(
+      (item) =>
+        `<li class="gallery__item"><a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+  </a></li>`
+    )
+    .join("");
+
+  galleryListEl.insertAdjacentHTML("beforeend", galleryItemsMarkUp);
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  let clickedGalleryImageLink = `${event.target.dataset.source}`;
+
+  showClickedFullSizeImage(clickedGalleryImageLink);
+}
+
+function showClickedFullSizeImage(link) {
+  const instance = basicLightbox.create(
+    `<img src="${link}" />
+        <a>Close</a>
+   `,
+    {
+      onShow: (instance) => {
+        instance.element().querySelector("a").onclick = instance.close;
+      },
+    }
+  );
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key == "Escape") {
+      instance.close();
+    }
+  });
+
+  instance.show();
+}
 
 console.log(galleryItems);
